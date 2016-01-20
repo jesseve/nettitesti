aaf9c654105edd006a865f62106860da187c0b24
jesseve <jesseverainen@gmail.com>
2016-01-20 13:41:05 +0200

    Project

---
+++ WebApplication1/WebApplication1/Models/TestiDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    class TestiDB : DbContext
+    {
+        public DbSet<MyClass> classes { get; set; }
+    }
+
+    class MyClass {
+        public string MyString { get; set; }
+        public void Asd() {
+            MyString = "";
+        }
+
+    }
+}
